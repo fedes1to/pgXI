bb5b7f435008a46df56b29fa8815142bc05ad368
fedesito <lbertitoyt@gmail.com>
2022-12-22 12:59:08 +0100

    fixed anims using enders script

---
+++ Assets/ProjectFixer/ProjectFixer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+
+public class ProjectFixer
+{
+	public static void Fix()
+	{
+		foreach (GameObject gobj in Resources.FindObjectsOfTypeAll<GameObject>())
+		{
+			if (gobj.GetComponent<MeshFilter>() && gobj.GetComponent<MeshCollider>())
+			{
+				gobj.GetComponent<MeshFilter>().sharedMesh = gobj.GetComponent<MeshCollider>().sharedMesh;
+			}
+		}
+	}
+    public static void FixAnims()
+    {
+        string str = Application.dataPath + "/AnimationClip";
+		string str2 = str + "/";
+        Debug.Log(str);
+		int amount = PlayerPrefs.GetInt("fixedAnimsAmount");
+		foreach (string file in Directory.GetFiles(str))
+		{
+			if (file.EndsWith("_" + amount + ".anim"))
+			{
+				amount++;
+			}
+		}
+        for (int i = 0; i < amount; i++)
+        {
+            Directory.CreateDirectory(str2 + i);
+            foreach (string file in Directory.GetFiles(str))
+            {
+                if (file.EndsWith("_" + i + ".anim"))
+                {
+                    File.Move(file, str2 + i + "/" + GetBitBefore(file.Substring(str.Length), "_" + i + ".anim") + ".anim");
+                }
+                if (file.EndsWith("_" + i + ".anim.meta"))
+                {
+                    File.Move(file, str2 + i + "/" + GetBitBefore(file.Substring(str.Length), "_" + i + ".anim.meta") + ".anim.meta");
+                }
+            }
+        }
+        PlayerPrefs.SetInt("fixedAnimsAmount", PlayerPrefs.GetInt("fixedAnimsAmount") + 100);
+    }
+    public static string GetBitBefore(string text, string end) 
+    {
+          var index = text.IndexOf(end);
+          if (index == -1) return text;
+
+           return text.Substring(0, index);
+    }
+}
